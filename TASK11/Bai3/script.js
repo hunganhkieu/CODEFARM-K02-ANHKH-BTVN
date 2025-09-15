c5759c173b6f14b81bfaff908569438dd0a82edc
anhphat123456789 <hunganh15610@gmail.com>
2025-09-15 22:01:30 +0700

    task 13

--- TASK11/Bai3/script.js
+++ TASK11/Bai3/script.js
@@ -4,8 +4,11 @@ const charCount = document.getElementById("charCount");
 const maxLength = 200;
 
 function updateCount() {
-  const numberChars = textArea.value.length;
-  const remainingChars = maxLength - numberChars;
+  // const numberChars = textArea.value.length;
+  if (textArea.value.length > maxLength) {
+    textArea.value = textArea.value.substring(0, maxLength);
+  }
+  const remainingChars = maxLength - textArea.value.length;
 
   const words = textArea.value.trim()
     ? textArea.value.trim().split(/\s+/).length
