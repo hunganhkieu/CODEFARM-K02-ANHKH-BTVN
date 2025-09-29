42da0005f2bee34b32c949feb15dc8bd8446fff4
anhphat123456789 <hunganh15610@gmail.com>
2025-09-29 18:47:04 +0700

    git

---
+++ TASK14/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Student from "./StudentList";
+import OutStandingStudent from "./OutStandingStudent";
+
+const App = () => {
+  return (
+    <>
+      <Student />
+      <OutStandingStudent />
+    </>
+  );
+};
+
+export default App;
